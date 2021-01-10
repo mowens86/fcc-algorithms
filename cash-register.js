27d47794435d2db9d4cebedc32560108cfddefeb
Mike Owens <mocoding86@gmail.com>
2021-01-10 15:55:45 -0800

    clean up

--- cash-register.js
+++ cash-register.js
@@ -33,66 +33,66 @@ checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUART
 
 
 function checkCashRegister(price, cash, cid) {
-    // Answer object
-    const answer = {
-      status: '',
-      change: []
-    }
-  
-    let changeDue = (cash - price) * 100;
-  
-    // Array of denominators
-    const denoms = {
-      "ONE HUNDRED": 10000,
-      "TWENTY": 2000,
-      "TEN": 1000,
-      "FIVE": 500,
-      "ONE": 100,
-      "QUARTER": 25,
-      "DIME": 10,
-      "NICKEL": 5,
-      "PENNY": 1
-    }
-  
-    // Re-organize array and mutiply by 100 to get accurate numbers due to float bugs
-    const register = cid.reverse().map(el => [el[0], Math.round(el[1] * 100)]);
-    const moneyInRegister = register.map(el => el[1]).reduce((sum, el) => sum + el);
-  
-    if ( changeDue < moneyInRegister ) answer.status = 'OPEN';
-    if ( changeDue === moneyInRegister ) answer.status = 'CLOSED';
-  
-    // Get empty denominators and store them
-    const emptyDenoms = register.map(elem => {
-      if (elem[1] === 0) {
-        return [elem[0], elem[1]];
-      }
-    });
-  
-    // Remove undefined and reverse order of array
-    const filteredEmptyDenoms = emptyDenoms.filter(el => el !== undefined).reverse();
-  
-    let partial;
-    answer.change = register.reduce((acc, elem) => {
-      partial = Math.min(elem[1], Math.floor(changeDue / denoms[elem[0]]) * denoms[elem[0]]);
-      if ( partial > 0) {
-        changeDue -= partial;
-        acc.push([elem[0], partial / 100])
-      } return acc;
-    }, []);
-  
-    // Check if change can't be given back and reset change to empty array
-    if ( changeDue > moneyInRegister || changeDue > 0) {
-      answer.status = 'INSUFFICIENT_FUNDS';
-      answer.change = [];
-    };
-  
-    // Check if status is closed then join the empty filtered denoms in order with the change at the beginning
-    if (answer.status === 'CLOSED') {
-      answer.change = answer.change.concat(filteredEmptyDenoms);
-    }
-      
-    return answer;
-    
+  // Answer object
+  const answer = {
+    status: '',
+    change: []
   }
+
+  let changeToGive = (cash - price) * 100;
+
+  // Object of denominators
+  const denoms = {
+    "ONE HUNDRED": 10000,
+    "TWENTY": 2000,
+    "TEN": 1000,
+    "FIVE": 500,
+    "ONE": 100,
+    "QUARTER": 25,
+    "DIME": 10,
+    "NICKEL": 5,
+    "PENNY": 1
+  }
+
+  // Re-organize array and mutiply by 100 to get accurate numbers due to float bugs
+  const register = cid.reverse().map(el => [el[0], Math.round(el[1] * 100)]);
+  const moneyInRegister = register.map(el => el[1]).reduce((sum, el) => sum + el);
+
+  if ( changeToGive < moneyInRegister ) answer.status = 'OPEN';
+  if ( changeToGive === moneyInRegister ) answer.status = 'CLOSED';
+
+  // Get empty denominators and store them
+  const emptyDenoms = register.map(elem => {
+    if (elem[1] === 0) {
+      return [elem[0], elem[1]];
+    }
+  });
+
+  // Remove undefined and reverse order of array
+  const filteredEmptyDenoms = emptyDenoms.filter(el => el !== undefined).reverse();
+
+  let partial;
+  answer.change = register.reduce((acc, elem) => {
+    partial = Math.min(elem[1], Math.floor(changeToGive / denoms[elem[0]]) * denoms[elem[0]]);
+    if ( partial > 0) {
+      changeToGive -= partial;
+      acc.push([elem[0], partial / 100])
+    } return acc;
+  }, []);
+
+  // Check if change can't be given back and reset change to empty array
+  if ( changeToGive > moneyInRegister || changeToGive > 0) {
+    answer.status = 'INSUFFICIENT_FUNDS';
+    answer.change = [];
+  };
+
+  // Check if status is closed then join the empty filtered denoms in order with the change at the beginning
+  if (answer.status === 'CLOSED') {
+    answer.change = answer.change.concat(filteredEmptyDenoms);
+  }
+    
+  return answer;
   
-  checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+}
+
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
